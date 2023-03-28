ccc1178293ca094665e3023b1088545ccbd8decf
Albert Shima <albertshima@gmail.com>
2023-03-28 23:21:05 +0200

    Initial

---
+++ src/test/java/com/cybertek/Day1/SimpleGetRequest.java
@@ -0,0 +1,26 @@
+package com.cybertek.Day1;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+    String url = "http://44.201.110.201:8000/api/spartans";
+
+
+
+    @Test
+    public void test1() {
+
+
+        Response response = RestAssured.get(url);
+        //print response status code
+        System.out.println(response.statusCode());
+
+        //print response body
+        response.prettyPrint();
+
+    }
+
+
+}
